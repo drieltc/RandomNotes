95886c5fe4b592381dee0c33c67f70ac92ba849a
Adriel  Torquato <adryeltc2001@gmail.com>
2023-07-01 22:15:47 -0300

    Update instrumento.js
    code is generally improved, created some things that will be of use later in the development.The big thing is that now a line of a string instrument that is not to be played in a given chord will have X's instead of a white space (or 0)

--- instrumento.js
+++ instrumento.js
@@ -8,124 +8,109 @@ class Instrumento {
 }
 
 class InstrumentoCordas extends Instrumento {
-  constructor(tuning, casas, flip = false) {
+  constructor(tuning, frets, flip = false) {
     super();
     this.tuning = tuning;
-    this.casas = casas;
+    this.frets = frets;
     this.flip = flip;
   }
 
   showPosition(rows, tuning, columns, position, flip = false) {
     const matrix = this.calculateMatrixPositions(rows, tuning, columns, position);
 
-    let chord = "";
-    chord = matrix.map((value) => {
-      let row = "";
-      if(!flip){
-        for (let i = 0; i < value.length; i++) {
-          if (value[i] === 1) {
-            row += "•|";
-          } else if (value[i] === 0) {
-            row += "_|";
-          }
-        }
-      }
+    let chord = matrix
+      .map(value => value.map(val => (val === '1' ? "•|" : val === 'X' ? "X|" : "-|")).join(""))
+      .join("\n");
 
-      return row;
-    });
-    chord = chord.join("\n");
     console.log(chord);
+    return chord;
   }
 
   calculateMatrixPositions(rows, tuning, columns, position) {
-    const pRow = [];
-    const pColumn = [];
-    let matrix = [];
+    const matrix = [];
+
+    const pRow = position.map((_, i) => tuning.indexOf(tuning[i]) + 1);
+    const pColumn = position;
 
     for (let i = 0; i < rows; i++) {
-      const [note, col] = position[i];
-      pRow.push(tuning.indexOf(note) + 1);
-      pColumn.push(col);
-    }
-
-    for (let i = 0; i <= rows; i++) {
-      let matrixRow = [];
+      const matrixRow = [];
 
       for (let j = 1; j <= columns; j++) {
-        matrixRow.push(j === pColumn[i - 1] && i === pRow[i - 1] ? 1 : 0);
+        matrixRow.push(j === pColumn[i] && i + 1 === pRow[i] ? '1' : "X" === pColumn[i] && i + 1 === pRow[i] ? "X" : '0');
       }
+
       matrix.push(matrixRow);
     }
 
-    matrix.shift();
     console.log(matrix);
     return matrix;
   }
 
   play(note, position) {
     console.log(`${note} = [${position}]`);
-    this.showPosition(this.tuning.length, this.tuning, this.casas, position);
+    this.showPosition(this.tuning.length, this.tuning, this.frets, position);
   }
 }
 
 class Ukulele extends InstrumentoCordas {
   tuning = ["G", "C", "E", "A"];
-  casas = 18;
+  frets = 18;
 
-  do(flip = false) {
-    const position = [["G", 0], ["C", 0], ["E", 0], ["A", 3]];
+  do(flip = false, showFrets = false, showNotes = false, see = 0) {
+    const position = [0, 0, 0, 3];
     this.play("Dó = C", position, flip);
   }
 
   re() {
-    const position = [["G", 2], ["C", 2], ["E", 2], ["A", 0]];
+    const position = [2, 2, 2, 0];
     this.play("Ré = D", position);
   }
 
   mi() {
-    const position = [["G", 1], ["C", 4], ["E", 0], ["A", 2]];
+    const position = [1, 4, 0, 2];
     this.play("Mi = E", position);
   }
 
   fa() {
-    const position = [["G", 2], ["C", 0], ["E", 1], ["A", 0]];
+    const position = [2, 0, 1, 0];
     this.play("Fá = F", position);
   }
 
   sol() {
-    const position = [["G", 0], ["C", 2], ["E", 3], ["A", 2]];
+    const position = [0, 2, 3, 2];
     this.play("Sol = G", position);
   }
 
   la() {
-    const position = [["G", 2], ["C", 1], ["E", 0], ["A", 0]];
+    const position = [2, 1, 0, 0];
     this.play("Lá = A", position);
   }
 
   si() {
-    const position = [["G", 4], ["C", 3], ["E", 2], ["A", 2]];
+    const position = [4, 3, 2, 2];
     this.play("Si = B", position);
   }
 }
 
 class Violao extends InstrumentoCordas {
-  tuning = ["E", "B", "G", "D", "A", "E"];
-  casas = 22;
+  tuning = ["E", "A", "D", "G", "B", "e"];
+  frets = 22;
+
+  do() {
+    const position = ["X", 3, 2, 0, 1, 0];
+    this.play("Dó = C", position);
+  }
 
   mi() {
-    const position = [["E", 0], ["B", 2], ["G", 2], ["D", 1], ["A", 0], ["E", 0]];
+    const position = [0, 2, 2, 1, 0, 0];
     this.play("Mi = E", position);
   }
 }
 
-const ukulele = new Ukulele();
-ukulele.do();
-ukulele.re();
-ukulele.mi();
-ukulele.fa();
-ukulele.sol();
-ukulele.la();
-ukulele.si();
+const v = new Violao
+v.do()
+v.mi()
 
-const violao = new Violao();
-violao.mi();
+const u = new Ukulele
+u.do()
+u.re()
