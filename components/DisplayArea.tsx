bf543af996057fbbff7a0f863b2359e742303a44
Adriel Torquato Costa <adriel.techinfo@gmail.com>
2023-08-02 19:16:07 -0300

    Controle de frequência finalizado, SettingsBar 90%

--- components/DisplayArea.tsx
+++ components/DisplayArea.tsx
@@ -1,60 +1,83 @@
-import React, { useState, useEffect } from 'react';
-import styles from "./DisplayArea.module.css"
+import React, { useState } from 'react';
+import styles from './DisplayArea.module.css';
+import Metronome from '../public/metronome.svg';
+function FreqControl() {
+  const [bpmValue, setBPMValue] = useState<number>(60);
 
-const Container = () => {
-    const [startTime, setStartTime] = useState(null);
-    const [elapsedTime, setElapsedTime] = useState(0);
-    const [isTimerRunning, setIsTimerRunning] = useState(false);
+  const activateButton = (value:number, sign:string) => {
+    const newBPMValue = sign === '-' ? bpmValue - value : bpmValue + value;
+    setBPMValue(Math.max(0, newBPMValue));
+  };
 
-    function handleButtonClick(){
-        if (isTimerRunning){
-            console.log("something")
-        }else{
-            console.log('something else')
-        }
+  const ButtonsValues = [1, 5, 10, 20];
+
+  const renderButtons = (sign:string) =>{
+    return ButtonsValues.map((value) => (
+      <button
+        key={value}
+        className={styles.SingleButtonBPM}
+        onClick={() => activateButton(value, sign)}
+        aria-label={`${sign === '-' ? 'Decrease' : 'Increase'} by ${value}`}
+      >
+        {`${sign === '-' ? '-' : '+'}${value}`}
+      </button>
+    ));
+  }
+
+  const handleMouseRest = () => {
+    console.log('Explicação BPM')
+  }
+
+  return (
+    <div id={styles.controllerBPM}>
+      <div className={styles.buttonsContainer}>{renderButtons('-')}</div>
+
+      <div id={styles.central}>
+        <div id={styles.bpm}>
+
+          <div id={styles.bpmValue}
+            onMouseOver={handleMouseRest}
+            //Continuar trabalhos aqui!!! A ideia é: se o mouse AINDA estiver por cima da div, criar renderizar um elemento falando um pouco sobre a unidade de medida usada (bpm)
+          >
+            {bpmValue}
+          </div>
+          <div id={styles.text}>bpm</div>
+        </div>
+
+        <button id={styles.customButton}>
+          {/*Será que eu consigo fazer uma animação que faça o metrônomo bater na frequência definida pelo usuário?*/}
+          <Metronome id={styles.metronome} />
+        </button>
+      </div>
+
+      <div className={styles.buttonsContainer}>{renderButtons('+')}</div>
+    </div>
+  );
+}
+
+export default function DisplayArea() {
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+
+  function handleButtonClick() {
+    if (isTimerRunning) {
+      console.log('something');
+    } else {
+      console.log('something else');
     }
+  }
 
-    return (
-      <div className="container">
+  return (
+    <>
+      <div className={styles.displayAreaContainer}>
         <div id={styles.notesContainer}>
           <div id={styles.notes}></div>
         </div>
         <button id={styles.start_stop_button} onClick={handleButtonClick}>
-            {isTimerRunning ? "Stop" : "Start"}
+          {isTimerRunning ? 'Stop' : 'Start'}
         </button>
       </div>
-    );
-};
 
-export default function DisplayArea() {
-    return (
-        <>
-            <Container />
-            <div id={styles.controller}>
-                <div>
-                    <ul>
-                        <li>-1</li>
-                        <li>-5</li>
-                        <li>-10</li>
-                        <li>-20</li>
-                    </ul>
-                </div>
-                <div id={styles.central}>
-                    <div id={styles.bpm}>
-                        <div>60</div>
-                        <div>bpm</div>
-                    </div>
-                    <div id={styles.custom}>custom</div>
-                </div>
-                <div>
-                    <ul>
-                        <li>+1</li>
-                        <li>+5</li>
-                        <li>+10</li>
-                        <li>+20</li>
-                    </ul>
-                </div>
-            </div>
-        </>
-    )
+      <FreqControl />
+    </>
+  );
 }
--- components/SettingsBar.tsx
+++ components/SettingsBar.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
-import { NumericFormat } from 'react-number-format';
+import React, { useState, useEffect } from 'react';
 import styles from './SettingsBar.module.css';
 
-const allItems = ['']
+let allSelectedItems:string[] = []  // Lift the state of allItems to the parent component
 
 function SingleOption({
   content,
@@ -14,13 +13,13 @@ function SingleOption({
   onClick: () => void;
 }) {
   return (
-    <div
+    <button
       className={`${styles.select} ${isSelected ? styles.selected : ''}`}
       onClick={onClick}
       id={content}
     >
       {content}
-    </div>
+    </button>
   );
 }
 
@@ -29,33 +28,53 @@ function GroupOptions({
   values,
   separator = true,
   multipleSelect = false,
+  defaultSelectedItems,
   onClick,
 }:{
   id: string;
   values:string[];
   separator?:boolean;
   multipleSelect?:boolean;
+  defaultSelectedItems?:string[]
   onClick?:() => void;
 }) {
 
-  const [selectedItems, setSelectedItems] = useState([]);
+  const [selectedItems, setSelectedItems] = useState(defaultSelectedItems || []);
   // Function to handle item selection
-  const handleItemSelect = (item:string) => {
-    if (!multipleSelect && values.length > 1){
-	    setSelectedItems([item]);
-    }
-    else if (!multipleSelect && values.length === 1) {
-      // If multipleSelect is false and values prop has only one element, behave like an on-off switch
+  const handleItemSelect = (item: string) => {
+    if (!multipleSelect && values.length > 1) {
+      setSelectedItems([item]);
+    } else if (!multipleSelect && values.length === 1) {
       setSelectedItems((prevSelectedItems) =>
-        prevSelectedItems.includes(item) ? []: [item]
+        prevSelectedItems.includes(item) ? [] : [item]
       );
     } else {
-      // Otherwise, toggle the item's selection when multipleSelect is true or values prop has more than one element
-      setSelectedItems((prevSelectedItems) => 
-      prevSelectedItems.includes(item) ? prevSelectedItems.filter((selectedItem) => selectedItem !== item) : [...prevSelectedItems, item]
+      // Check if it is the last selected item and if it's being deselected
+      if (selectedItems.length === 1 && selectedItems.includes(item)) {
+        return; // Prevent deselecting the last selected item
+      }
+  
+      setSelectedItems((prevSelectedItems) =>
+        prevSelectedItems.includes(item)
+          ? prevSelectedItems.filter((selectedItem) => selectedItem !== item)
+          : [...prevSelectedItems, item]
       );
     }
   };
+  
+
+  // Create a new function that calls both onClick and handleItemSelect
+  const handleClick = (value:string) => {
+    if (onClick) {
+      onClick(); // Call the onClick function if it exists
+    }
+    // Always call handleItemSelect to handle item selection
+    handleItemSelect(value);
+  };  useEffect(() => {
+    allSelectedItems = selectedItems; // Update the allSelecteedItems variable whenever selectedItems changes
+    console.log(allSelectedItems)
+    //Ainda não tá funcionando da maneira que eu quero. Provavelmente eu crie um useState para cada menu e coloque esses estados como as dependências desse useEffect aqui, mas n sei ainda
+  }, [selectedItems]);
 
   return (
     <>
@@ -65,7 +84,7 @@ function GroupOptions({
             key={value}
             content={value}
             isSelected={selectedItems.includes(value)} // Check if the item is in the selectedItems array
-            onClick={() => handleItemSelect(value)} // Pass the function to handle item selection
+            onClick={() => handleClick(value)} // Pass the function to handle item selection
           />
         ))}
       </div>
@@ -78,70 +97,23 @@ function Separator() {
   return <div className={styles.separator}></div>;
 }
 
-function FreqControl(){
-  const [bpmValue, setBPMValue] = useState('60')
-  const [prevBPMValue, setPrevBPMValue] = useState ('')
-
-  const activateButton = (value:string, sign:string) =>{
-    if(sign == '-'){
-      if(parseInt(bpmValue) - parseInt(value) >= 0){
-        setBPMValue(`${parseInt(bpmValue) - parseInt(value)}`)
-      }
-      else{
-        setBPMValue('0')
-      }
-    }
-    if (sign == '+'){
-      setBPMValue(`${parseInt(bpmValue) + parseInt(value)}`)
-    }
-  }
-
-  const handleChange = (event) => {
-    let inputValue = event.target.value
-    const maxArraySize = 3;
-    const numbersArray = inputValue
-      .slice(-maxArraySize) // Take the last 3 characters from the input
-      .padStart(maxArraySize, '') // Pad the string to the left with '' if less than 3 characters
-      .split('') // Convert the string to an array of characters
-       
-      console.log(numbersArray)
-    setBPMValue(numbersArray.join('')); // Convert the array back to a string to be rendered on screen
-  };
-  
-
-  const valuesUp = ['10', '20']
-  const valuesDown = ['20', '10']
-  return(
-    <>
-    <div id={styles.bpm}>
-      <div id={styles.buttons}>
-        {valuesDown.map((value:string) => (<SingleOption key = {value} content={`-${value}`} onClick={() => activateButton(value, '-')} />))}
-
-        <input type="number" inputMode="numeric" id={styles.input} value={bpmValue} min={0} onChange={handleChange}/>
-        {/*<div id={styles.numberInput}>{bpmValue}<div>bpm</div></div>*/}
-
-        {valuesUp.map((value:string) => (<SingleOption key = {value} content={`+${value}`} onClick={() => activateButton(value, '+')} />))}
-      </div>
-    </div>
-    <Separator />
-    </>
-  )
-}
-
 export default function SettingsBar() {
 
   const renderIntruments = () =>{
     console.log('Renderizar barra de instrumentos')
+    //Agora é desenvolver essa função agui
   }
 
   return (
     <div className={styles.settings}>
       <div className={styles.rowSettings}>
-        <GroupOptions id="playMode" values={["timer", "zen"]} />
-        <GroupOptions id="displayMode" values={["notes", "chords"]} />
-        <GroupOptions id="whatToDisplay" values={["partiture", "name", "symbol"]} multipleSelect={true} />
+        <GroupOptions id="playMode" values={["timer", "zen"]} defaultSelectedItems={['zen']}/>
+        <GroupOptions id="displayMode" values={["notes", "chords"]} defaultSelectedItems={['notes']}/>
+        <GroupOptions id="whatToDisplay" values={["partiture", "name", "symbol"]} defaultSelectedItems={['symbol']} multipleSelect={true} />
         <GroupOptions id="instrumentSettings" values={["instruments"]} separator={false} onClick={renderIntruments}/>
       </div>
     </div>
   );
 }
+
+export {allSelectedItems}
