18a71e376d6b3c4cf766f7d77aa993a7c42da368
Adriel Torquato Costa <adriel.techinfo@gmail.com>
2023-08-18 09:07:02 -0300

    feat(music): create the instuments.js file

---
+++ music/instruments.js
@@ -0,0 +1,28 @@
+const instruments = {
+    Guitar:{
+        strings: 6,
+        frets: 22,
+        tunning: ['E', 'A', 'D', 'G', 'B', 'E'],
+    },
+
+    Ukulele:{
+        strings: 4,
+        frets: 18,
+        tunning: ['G', 'C', 'E', 'A'],
+    },
+
+    Bass:{
+        strings: 4,
+        frets: 999,
+        tunning: ['E', 'A', 'D', 'G'],
+    },
+
+    Contrabass:{
+        strings: 4,
+        frets: 999,
+        tunning: ['G', 'C', 'E', 'A'],
+    },
+    Piano_Keyboard:{},
+}
+
+export default instruments
